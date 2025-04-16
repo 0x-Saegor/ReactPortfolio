49a7c5e96a256a66f75b78d11d79d26fd2e143b6
arthu <arthurleg29@gmail.com>
2025-04-16 17:03:48 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ReactPortfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
